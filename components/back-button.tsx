d6fb0ba2421726f11f692414cc7963f709935efc
przish <irish_may_pureza@dlsl.edu.ph>
2026-08-05 15:40:04 +0800

    style: back button shadow when pressing

--- components/back-button.tsx
+++ components/back-button.tsx
@@ -1,20 +1,23 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import { router } from "expo-router";
-import { View, type GestureResponderEvent, Pressable } from "react-native"
+import { View, type GestureResponderEvent, Pressable, StyleSheet } from "react-native"
 
 type BackButtonProps = {
     onPress?: (event: GestureResponderEvent) => void;
 };
 
 export default function BackButton({ onPress }: BackButtonProps) {
-    const handlePress = (event: GestureResponderEvent) => {
-        if (onPress) return onPress(event);
-        router.back();
-    };
+
 
     return (
         <View className="w-full mt-10">
-            <Pressable onPress={handlePress}>
+            <Pressable 
+                onPress={() => {
+                    console.log("back button pressed")
+                    router.back()
+                }}
+                className="items-center justify-center active:bg-gray-200 overflow-hidden w-12 h-12 rounded-full"
+            >
                 <MaterialIcons
                     name="arrow-back-ios"
                     color={"#AF2219"}
