4a9aa239fbbe93fa4a92487f384c23c371399572
przish <irish_may_pureza@dlsl.edu.ph>
2026-08-05 15:12:47 +0800

    refactor: when pressing back button

--- components/back-button.tsx
+++ components/back-button.tsx
@@ -1,20 +1,26 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import { router } from "expo-router";
-import { View, Text } from "react-native"
+import { View, type GestureResponderEvent, Pressable } from "react-native"
 
-export default function BackButton(){
-    return(
+type BackButtonProps = {
+    onPress?: (event: GestureResponderEvent) => void;
+};
+
+export default function BackButton({ onPress }: BackButtonProps) {
+    const handlePress = (event: GestureResponderEvent) => {
+        if (onPress) return onPress(event);
+        router.back();
+    };
+
+    return (
         <View className="w-full mt-10">
-            <MaterialIcons
-                name="arrow-back-ios"
-                color={"#AF2219"}
-                size={24}
-                onPress={() => {
-                    console.log("go back to email");
-                    router.back();
-                    
-                }}
-            />
+            <Pressable onPress={handlePress}>
+                <MaterialIcons
+                    name="arrow-back-ios"
+                    color={"#AF2219"}
+                    size={24}
+                />
+            </Pressable>
         </View>
     )
 }
